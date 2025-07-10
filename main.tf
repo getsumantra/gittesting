391baff711f11c103df32f96c52c7c7b196238e4
unknown <primes461@gmail.com>
2025-07-10 23:24:41 +0530

    rgadded

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg01" {
   name = "rg-sumantra"
   location = "central india"
 }
+
+resource "azurerm_resource_group" "rg02" {
+  name     = "rg-sumantra-2"
+  location = "central india"
+}
